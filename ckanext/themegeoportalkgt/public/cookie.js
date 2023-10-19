85522e2916a862de698a59ef7dd30b56bcc0fb4f
gdmkreisgt <gis@kreis-guetersloh.de>
2023-10-19 14:10:32 +0200

    Update cookie.js

--- ckanext/themegeoportalkgt/public/cookie.js
+++ ckanext/themegeoportalkgt/public/cookie.js
@@ -3,7 +3,7 @@ function setCookie(cname, cvalue, exdays) {
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    checkCookie(cname);
+    banner.style.display = "none";
 }
 function getCookie(cname) {
     let name = cname + "=";
