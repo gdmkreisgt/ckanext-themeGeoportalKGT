9a88d56ee27505ce5d3c0b280f2261b7f997348e
gdmkreisgt <gis@kreis-guetersloh.de>
2023-10-20 09:22:40 +0200

    Changed Cookie to Dom Created

--- ckanext/themegeoportalkgt/public/cookie.js
+++ ckanext/themegeoportalkgt/public/cookie.js
@@ -22,8 +22,38 @@ function getCookie(cname) {
 }
 function checkCookie(cname) {
     let cookie = getCookie(cname);
-    let banner = document.getElementById("cookie-banner-main");
     if (cookie === "") {
+        createCookieBanner();
         banner.classList.add("cookie-banner-bg-not-set")
     }
+}
+function createCookieBanner(){
+    let main = document.getElementById("cookie-banner-main");
+    let banner = document.createElement("DIV");
+    banner.classList.add("cookie-consent-banner");
+    let inner = document.createElement("DIV");
+    inner.classList.add("cookie-consent-banner__inner");
+    let copy = document.createElement("DIV");
+    copy.classList.add("cookie-consent-banner__copy");
+    let header = document.createElement("DIV");
+    header.classList.add("cookie-consent-banner__header");
+    let headerContent = document.createTextNode("Diese Website verwendet Cookies");
+    header.appendChild(headerContent)
+    let description = document.createElement("DIV");
+    description.classList.add("cookie-consent-banner__description");
+    let descriptionContent = document.createTextNode("Wir verwenden Cookies, um eine reibungslose Funktionalität der Website garantieren zu können.</br>Es werden <strong>keine</strong> Daten zu Marketing- oder Analysezwecken gesammelt und gespeichert.");
+    description.appendChild(descriptionContent)
+    let actions = document.createElement("DIV");
+    actions.classList.add("cookie-consent-banner__actions");
+    let btn = document.createElement("BUTTON");
+    btn.classList.add("cookie-consent-banner__cta");
+    btn.onclick = setCookie('ODKGTCookie',true,30);
+    btn.textContent = "OK";
+    actions.appendChild(btn);
+    copy.appendChild(header);
+    copy.appendChild(description);
+    inner.appendChild(copy);
+    inner.appendChild(actions);
+    banner.appendChild(inner);
+    main.appendChild(main)
 }
