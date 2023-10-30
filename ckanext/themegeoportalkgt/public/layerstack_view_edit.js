75391c4f264c9c45f61afef616a91930666fee93
gdmkreisgt <gis@kreis-guetersloh.de>
2023-10-30 16:59:15 +0100

    Style edit of layer stack button in geoview

---
+++ ckanext/themegeoportalkgt/public/layerstack_view_edit.js
@@ -0,0 +1,7 @@
+let stacked_layers = document.getElementsByClassName("stacked-layers");
+
+stacked_layers.textContent = "";
+
+let layer1 = document.createElement("DIV");
+layer1.classList.add("layer-1");
+stacked_layers.appendChild(layer1);
