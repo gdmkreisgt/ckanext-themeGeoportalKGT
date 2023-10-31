4f1c83727986d8f35ffd960ca0dfbc031fea5446
gdmkreisgt <gis@kreis-guetersloh.de>
2023-10-31 08:38:55 +0100

    access iframe

--- ckanext/themegeoportalkgt/public/layerstack_view_edit.js
+++ ckanext/themegeoportalkgt/public/layerstack_view_edit.js
@@ -1,7 +1,15 @@
-let stacked_layers = document.getElementsByClassName("stacked-layers");
+let iframe = document.querySelector('[title="Data Viewer"]');
+let innerDoc = iframe.contentDocument || iframe.contentWindow.document;
 
+let cssLink = innerDoc.createElement("link");
+cssLink.href = "/layerstack.css"; 
+cssLink.rel = "stylesheet"; 
+cssLink.type = "text/css"; 
+frames['iframe1'].innerDoc.head.appendChild(cssLink);
+
+let stacked_layers = innerDoc.getElementsByClassName("stacked-layers");
 stacked_layers.textContent = "";
 
-let layer1 = document.createElement("DIV");
+let layer1 = innerDoc.createElement("DIV");
 layer1.classList.add("layer-1");
 stacked_layers.appendChild(layer1);
