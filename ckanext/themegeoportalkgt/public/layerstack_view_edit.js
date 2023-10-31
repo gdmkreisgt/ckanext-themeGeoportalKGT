925df189e740f4d7b38a29fb7f39821d374d18be
gdmkreisgt <gis@kreis-guetersloh.de>
2023-10-31 13:57:45 +0100

    added animation

--- ckanext/themegeoportalkgt/public/layerstack_view_edit.js
+++ ckanext/themegeoportalkgt/public/layerstack_view_edit.js
@@ -5,12 +5,4 @@ window.addEventListener('load', function () {
     cssLink.rel = "stylesheet"; 
     cssLink.type = "text/css"; 
     iframe.head.appendChild(cssLink);
-    let stacked_layers = iframe.getElementsByClassName("stacked-layers");
-    console.log(stacked_layers[0])
-    stacked_layers[0].textContent = "";
-    
-
-    let layer1 = iframe.createElement("DIV");
-    layer1.classList.add("layer-1");
-    stacked_layers[0].appendChild(layer1);
 })
