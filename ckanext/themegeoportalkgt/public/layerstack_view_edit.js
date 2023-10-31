b92724c9ef699e4203b94fbecb68c685634737c4
gdmkreisgt <gis@kreis-guetersloh.de>
2023-10-31 11:00:54 +0100

    Update layerstack_view_edit.js

--- ckanext/themegeoportalkgt/public/layerstack_view_edit.js
+++ ckanext/themegeoportalkgt/public/layerstack_view_edit.js
@@ -1,19 +1,16 @@
 window.addEventListener('load', function () {
-    let iframe = document.querySelector('[title="Data viewer"]');
-    console.log(iframe)
-    let innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-    console.log(innerDoc)
-
+    let iframe = frames[0].document;
     let cssLink = document.createElement("link");
     cssLink.href = "/layerstack.css"; 
     cssLink.rel = "stylesheet"; 
     cssLink.type = "text/css"; 
-    frames[0].document.head.appendChild(cssLink);
-    let stacked_layers = innerDoc.getElementsByClassName("stacked-layers");
-    stacked_layers[0].textContent = "";
+    iframe.head.appendChild(cssLink);
+    let stacked_layers = iframe.getElementsByClassName("stacked-layers");
     console.log(stacked_layers[0])
+    stacked_layers[0].textContent = "";
+    
 
-    let layer1 = innerDoc.createElement("DIV");
+    let layer1 = iframe.createElement("DIV");
     layer1.classList.add("layer-1");
     stacked_layers[0].appendChild(layer1);
 })
